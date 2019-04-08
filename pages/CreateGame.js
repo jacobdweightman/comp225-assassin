4f3cb4a10c9e8f206310b90f3a32d88b773fefa6
Jacob Weightman <jacobdweightman@gmail.com>
2019-04-08 16:45:09 -0500

    implemented create game API

--- pages/CreateGame.js
+++ pages/CreateGame.js
@@ -4,21 +4,46 @@ import { LinearGradient } from 'expo';
 import baseStyle from '../UI/defaultStyles/DefaultStyle';
 import Palette from '../UI/defaultStyles/Palette';
 
+import global from '../Global';
+
 
 export default class CreateGame extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  create() {
-    if (global.gameName.length < 2) { // basic input validation
+  async create() {
+    // validate input
+    if (global.gameName.length < 2) {
       Alert.alert("Please enter a valid game name");
+      return;
     }
-    else {
-      global.creator = true;
-      global.code = 1234; // probably something else
-      const {navigate} = this.props.navigation;
-      return navigate("join2");
+
+    try {
+      let response = await fetch(global.BASE_URL + "creator_access/create_game", {
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json",
+        },
+        body: JSON.stringify({
+          game_name: global.gameName,
+          game_rules: global.gameRules,
+        }),
+      });
+
+      if(response.status === 200) {
+        let json = await response.json();
+        global.creator = true;
+        global.code = json.game_code;
+        return this.props.navigation.navigate("join2");
+      } else {
+        alert("A network error occurred.");
+        console.log(response);
+      }
+
+    } catch(error) {
+      alert("An error occured while creating your game.");
+      console.log(error);
     }
   }
 
