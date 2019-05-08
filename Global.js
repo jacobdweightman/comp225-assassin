ec186a0e91a2268017558e0eb5760b6c2d9664cc
Jacob Weightman <jacobdweightman@gmail.com>
2019-05-07 22:28:32 -0500

    changed base url to point to Heroku server

--- Global.js
+++ Global.js
@@ -2,7 +2,7 @@ import { SecureStore } from 'expo';
 
 class Global {
     constructor() {
-        this.BASE_URL = "http://10.0.2.2:5000/"; //"https://elcoanja.herokuapp.com/"
+        this.BASE_URL = "https://elcoanja.herokuapp.com/"; //"http://10.0.2.2:5000/";
         // NOTE: this is the loopback address for Android Studio, using localhost / 127.0.0.1 will not work
         // see:   https://developer.android.com/studio/run/emulator-networking
         this.accessToken;
